c525570bde3f438fa16afa3d89ef008b73b6e588
Cristian <asdf@correo.com>
2014-05-05 10:07:22 +0200

    Agregados los metodos que habia en la clase Game que le corresponden ahora a la clase Player

--- Player.java
+++ Player.java
@@ -15,4 +15,46 @@ public class Player
         this.currentRoom = currentRoom;
         previusRoom = new Stack<>();
     }
+
+    /** 
+     * Try to go in one direction. If there is an exit, enter
+     * the new room, otherwise print an error message.
+     */
+    private void goRoom(Command command) 
+    {
+        if(!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+        Room nextRoom = currentRoom.getExit(command.getSecondWord());
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        }
+        else {
+            previusRoom.push(currentRoom);
+            currentRoom = nextRoom;
+            printLocationInfo();
+        }
+    }
+
+    private void printLocationInfo(){
+        System.out.println(currentRoom.getLongDescription() + "\n");
+    }
+
+    private void eat(){
+        System.out.println("You have eaten now and you are not hungry any more");
+    }
+
+    private void back(){
+        if(!previusRoom.empty()){
+            currentRoom = previusRoom.pop();
+            printLocationInfo();
+        }
+        else{
+            System.out.println("No se puede ir más atrás. Ya está en el inicio");
+        }
+
+    }
 }
